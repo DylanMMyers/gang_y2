b21ffaa1ea0da6db75c2fcb03b7ea9a3cac5b252
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2025-09-29 19:58:49 -0500

    fixed db init

--- db_init.sql
+++ db_init.sql
@@ -5,6 +5,14 @@ CREATE TABLE orders (
     orderCost INT
 );
 
+CREATE TABLE menuItems (
+    itemID INT PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    price FLOAT,
+    calories INT
+);
+
 CREATE TABLE orderItems (
     orderDetailID INT PRIMARY KEY,
     orderID INT,
@@ -12,12 +20,4 @@ CREATE TABLE orderItems (
     itemPrice FLOAT,
     FOREIGN KEY (orderID) REFERENCES orders(orderID),
     FOREIGN KEY (itemID) REFERENCES menuItems(itemID)
-);
-
-CREATE TABLE menuItems (
-    itemID INT PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    price FLOAT,
-    calories INT
 );
