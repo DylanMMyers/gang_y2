7466447eac8221aef6ce0bc76cf5057e355878c5
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2025-09-30 15:26:26 -0500

    better gen

--- gen.py
+++ gen.py
@@ -9,8 +9,17 @@ path = f"gen_{str(name)}.csv"
 rand_data = [["orderID", "orderDate", "orderTime", "orderCost"]]
 
 for i in range(0, rows):
-    new_date = str(random.randint(2000, 2025)) + '-' + str(random.randint(10, 12)) + '-' + str(random.randint(10, 30))
-    new_time = str(random.randint(10, 12)) + ':' + str(random.randint(10, 59)) + ':' + str(random.randint(10, 59))
+    twelve = str(random.randint(1, 12)) 
+    if len(twelve) == 1:
+        twelve = "0" + twelve
+    thirty = str(random.randint(1, 28))
+    if len(thirty) == 1:
+        thirty = "0" + thirty
+    sixty = str(random.randint(0, 59))
+    if len(sixty) == 1:
+        sixty = "0" + sixty
+    new_date = str(random.randint(2000, 2025)) + '-' + twelve + '-' + thirty
+    new_time = twelve + ':' + sixty + ':' + sixty
     rand_data.append([i + 2, new_date, new_time, random.randint(1, 20)])
 
 with open(path, 'w', newline='') as csvfile:
