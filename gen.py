ed14a8e6d7e9bdb058c4fe422ea994ffdb579bf7
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2025-09-30 14:50:15 -0500

    random order data gen

---
+++ gen.py
@@ -0,0 +1,19 @@
+import random
+import csv
+
+# change this to get diff rows and names
+rows = 1000
+name = 1
+path = f"gen_{str(name)}.csv"
+
+rand_data = [["orderID", "orderDate", "orderTime", "orderCost"]]
+
+for i in range(0, rows):
+    new_date = str(random.randint(2000, 2025)) + '-' + str(random.randint(10, 12)) + '-' + str(random.randint(10, 30))
+    new_time = str(random.randint(10, 12)) + ':' + str(random.randint(10, 59)) + ':' + str(random.randint(10, 59))
+    rand_data.append([i + 2, new_date, new_time, random.randint(1, 20)])
+
+# with open(path)
+with open(path, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(rand_data)
