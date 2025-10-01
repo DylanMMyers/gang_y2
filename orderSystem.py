75f5e34384ab7b0b8d84aa91047333d3bddc9432
MgSmith04 <MgSmith04@outlook.com>
2025-09-30 19:51:42 -0500

    working on order system

--- orderSystem.py
+++ orderSystem.py
@@ -5,23 +5,31 @@
 import datetime
 
 class Item:
-    def __init__(self, itemID, size, ice, boba, cost):
-        self.itemID = itemID # will reference the menuItems table to get attributes
-        self.size = size   # 0 = small, 1 = medium, 2 = large
-        self.ice = ice    # 0 = no ice, 1 = light ice, 2 = regular ice, 3 = full ice
-        self.boba = boba   # number of boba scoops
-        self.cost = cost # the cost (in dollars) of the item in its current state
+    def __init__(self, itemID:int, size:int, ice:int, boba:int, price:float):
+        self.itemID = itemID    # will reference the menuItems table to get attributes
+        self.size = size        # 0 = small, 1 = medium, 2 = large
+        self.ice = ice          # 0 = no ice, 1 = light ice, 2 = regular ice, 3 = full ice
+        self.boba = boba        # number of boba scoops
+        self.price = price      # the price (in dollars) of the item in its current state
 
 
 class Order:
     def __init__(self, orderID, items):
         self.orderID = orderID                  # id of order, will be assigned sequentially
-        self.items = items                      # list of items on order
-                                                    # in the database this will reference an items table
-        self.date = datetime.date.today()       # date stamps the order when it is created
+        self.items = items                      # list of items on order # in the database this will reference an items table
+        self.price = self.set_price()           # total price of the order, will need to be updated when something is added
+        # self.date = datetime.date.today()     # date stamps the order when it is created # this is redundant
         self.time = datetime.datetime.now()     # time stamps when the order was created
-        
 
+    def set_price(self):
+        # gets the price for all items in the order, updates the order's price, and returns it
+        totalPrice = 0
+        for i in range(len(self.items)):
+            totalPrice += self.items[i].price
+        self.price = totalPrice
+        return totalPrice
+        
+# simple tests
 # making sure the classes sort of work
 testItem1 = Item(1, 2, 2, 1, 5.99)
 testItem2 = Item(2, 2, 3, 1, 4.99)
@@ -32,4 +40,6 @@ print(len(itemsList))
 
 testOrder = Order(1, itemsList)
 
-print(testOrder.items[2].cost)
+print(testOrder.time)
+# testOrder.set_price()
+print(testOrder.price)
