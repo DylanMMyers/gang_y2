cad0d1a4535dcd858a38a6102b43892af786fb6d
KaileeCho <kcboss102@tamu.edu>
2025-10-01 19:47:48 -0500

    fixed "pearl"

--- menuItemInventory_seed.sql
+++ menuItemInventory_seed.sql
@@ -1,26 +1,26 @@
 INSERT INTO menuItemInventory (itemID, inventoryItem, qtyPerDrink) VALUES
 -- 1 Classic Pearl Milk Black Tea
-(1,'Black Tea Leaves',8.0),(1,'Milk Powder',30.0),(1,'Sugar Syrup',20.0),(1,'Pearls',60.0),
+(1,'Black Tea Leaves',8.0),(1,'Milk Powder',30.0),(1,'Sugar Syrup',20.0),(1,'Pearl',60.0),
 (1,'Plastic Cups 16oz',1.0),(1,'Cup Lids',1.0),(1,'Straws',1.0),
 
 -- 2 Classic Pearl Milk Green Tea
-(2,'Green Tea Leaves',8.0),(2,'Milk Powder',30.0),(2,'Sugar Syrup',20.0),(2,'Pearls',60.0),
+(2,'Green Tea Leaves',8.0),(2,'Milk Powder',30.0),(2,'Sugar Syrup',20.0),(2,'Pearl',60.0),
 (2,'Plastic Cups 16oz',1.0),(2,'Cup Lids',1.0),(2,'Straws',1.0),
 
 -- 3 Classic Pearl Milk Oolong Tea
-(3,'Oolong Tea Leaves',8.0),(3,'Milk Powder',30.0),(3,'Sugar Syrup',20.0),(3,'Pearls',60.0),
+(3,'Oolong Tea Leaves',8.0),(3,'Milk Powder',30.0),(3,'Sugar Syrup',20.0),(3,'Pearl',60.0),
 (3,'Plastic Cups 16oz',1.0),(3,'Cup Lids',1.0),(3,'Straws',1.0),
 
 -- 4 Honey Pearl Milk Black Tea
-(4,'Black Tea Leaves',8.0),(4,'Milk Powder',30.0),(4,'Honey Syrup',25.0),(4,'Pearls',60.0),
+(4,'Black Tea Leaves',8.0),(4,'Milk Powder',30.0),(4,'Honey Syrup',25.0),(4,'Pearl',60.0),
 (4,'Plastic Cups 16oz',1.0),(4,'Cup Lids',1.0),(4,'Straws',1.0),
 
 -- 5 Honey Pearl Milk Green Tea
-(5,'Green Tea Leaves',8.0),(5,'Milk Powder',30.0),(5,'Honey Syrup',25.0),(5,'Pearls',60.0),
+(5,'Green Tea Leaves',8.0),(5,'Milk Powder',30.0),(5,'Honey Syrup',25.0),(5,'Pearl',60.0),
 (5,'Plastic Cups 16oz',1.0),(5,'Cup Lids',1.0),(5,'Straws',1.0),
 
 -- 6 Honey Pearl Milk Oolong Tea
-(6,'Oolong Tea Leaves',8.0),(6,'Milk Powder',30.0),(6,'Honey Syrup',25.0),(6,'Pearls',60.0),
+(6,'Oolong Tea Leaves',8.0),(6,'Milk Powder',30.0),(6,'Honey Syrup',25.0),(6,'Pearl',60.0),
 (6,'Plastic Cups 16oz',1.0),(6,'Cup Lids',1.0),(6,'Straws',1.0),
 
 -- 7 Coffee Creama
@@ -32,15 +32,15 @@ INSERT INTO menuItemInventory (itemID, inventoryItem, qtyPerDrink) VALUES
 (8,'Plastic Cups 16oz',1.0),(8,'Cup Lids',1.0),(8,'Straws',1.0),
 
 -- 9 Hokkaido Pearl
-(9,'Black Tea Leaves',8.0),(9,'Milk Powder',30.0),(9,'Toffee Syrup',20.0),(9,'Pearls',60.0),
+(9,'Black Tea Leaves',8.0),(9,'Milk Powder',30.0),(9,'Toffee Syrup',20.0),(9,'Pearl',60.0),
 (9,'Plastic Cups 16oz',1.0),(9,'Cup Lids',1.0),(9,'Straws',1.0),
 
 -- 10 Thai Pearl Milk Tea
-(10,'Thai Tea Mix',10.0),(10,'Milk Powder',30.0),(10,'Sugar Syrup',20.0),(10,'Pearls',60.0),
+(10,'Thai Tea Mix',10.0),(10,'Milk Powder',30.0),(10,'Sugar Syrup',20.0),(10,'Pearl',60.0),
 (10,'Plastic Cups 16oz',1.0),(10,'Cup Lids',1.0),(10,'Straws',1.0),
 
 -- 11 Taro Pearl Milk Tea
-(11,'Taro Powder',30.0),(11,'Milk Powder',25.0),(11,'Sugar Syrup',20.0),(11,'Pearls',60.0),
+(11,'Taro Powder',30.0),(11,'Milk Powder',25.0),(11,'Sugar Syrup',20.0),(11,'Pearl',60.0),
 (11,'Plastic Cups 16oz',1.0),(11,'Cup Lids',1.0),(11,'Straws',1.0),
 
 -- 12 Mango Green Milk Tea
@@ -48,15 +48,15 @@ INSERT INTO menuItemInventory (itemID, inventoryItem, qtyPerDrink) VALUES
 (12,'Plastic Cups 16oz',1.0),(12,'Cup Lids',1.0),(12,'Straws',1.0),
 
 -- 13 Golden Retriever
-(13,'Black Tea Leaves',8.0),(13,'Creamer',25.0),(13,'Sugar Syrup',20.0),(13,'Pearls',60.0),
+(13,'Black Tea Leaves',8.0),(13,'Creamer',25.0),(13,'Sugar Syrup',20.0),(13,'Pearl',60.0),
 (13,'Plastic Cups 16oz',1.0),(13,'Cup Lids',1.0),(13,'Straws',1.0),
 
 -- 14 Coconut Pearl Milk Black Tea
-(14,'Black Tea Leaves',8.0),(14,'Milk Powder',30.0),(14,'Coconut Syrup',25.0),(14,'Pearls',60.0),
+(14,'Black Tea Leaves',8.0),(14,'Milk Powder',30.0),(14,'Coconut Syrup',25.0),(14,'Pearl',60.0),
 (14,'Plastic Cups 16oz',1.0),(14,'Cup Lids',1.0),(14,'Straws',1.0),
 
 -- 15 Coconut Pearl Milk Green Tea
-(15,'Green Tea Leaves',8.0),(15,'Milk Powder',30.0),(15,'Coconut Syrup',25.0),(15,'Pearls',60.0),
+(15,'Green Tea Leaves',8.0),(15,'Milk Powder',30.0),(15,'Coconut Syrup',25.0),(15,'Pearl',60.0),
 (15,'Plastic Cups 16oz',1.0),(15,'Cup Lids',1.0),(15,'Straws',1.0),
 
 -- 16 Classic Black Tea
