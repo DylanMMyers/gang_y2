6543da27a784938051e99f8b45ae85a02a0c8515
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2025-10-01 19:29:55 -0500

    generated order data

--- gen.py
+++ gen.py
@@ -13,29 +13,35 @@ for i in range(0, rows):
     twelve = str(random.randint(1, 12)) 
     if len(twelve) == 1:
         twelve = "0" + twelve
-    thirty = str(random.randint(0, 28))
+    thirty = str(random.randint(1, 28))
     if len(thirty) == 1:
         thirty = "0" + thirty
-    sixty = str(random.randint(0, 59))
+    sixty = str(random.randint(1, 59))
     if len(sixty) == 1:
         sixty = "0" + sixty
+    sixty2 = str(random.randint(1, 59))
+    if len(sixty2) == 1:
+        sixty2 = "0" + sixty2
     new_date = str(random.randint(2025, 2025)) + '-' + twelve + '-' + thirty
-    new_time = twelve + ':' + sixty + ':' + sixty
+    new_time = twelve + ':' + sixty + ':' + sixty2
     rand_data.append([new_date, new_time, str(random.randint(1, 20)) + '.' + str(random.randint(0, 9)) + str(random.randint(0, 9))])
 
 for i in range(0, 22000):
     twelve = "08"
-    # twelve = str(random.randint(1, 12)) 
+    # twelve = str(random.randint(1, 12))
     # if len(twelve) == 1:
     #     twelve = "0" + twelve
     thirty = str(random.randint(26, 27))
     if len(thirty) == 1:
         thirty = "0" + thirty
-    sixty = str(random.randint(0, 59))
+    sixty = str(random.randint(1, 59))
     if len(sixty) == 1:
         sixty = "0" + sixty
+    sixty2 = str(random.randint(1, 59))
+    if len(sixty2) == 1:
+        sixty2 = "0" + sixty2
     new_date = str(random.randint(2025, 2025)) + '-' + twelve + '-' + thirty
-    new_time = twelve + ':' + sixty + ':' + sixty
+    new_time = twelve + ':' + sixty + ':' + sixty2
     rand_data.append([new_date, new_time, str(random.randint(1, 20)) + '.' + str(random.randint(0, 9)) + str(random.randint(0, 9))])
 
 for i in range(0, 22000):
@@ -46,11 +52,14 @@ for i in range(0, 22000):
     thirty = str(random.randint(1, 2))
     if len(thirty) == 1:
         thirty = "0" + thirty
-    sixty = str(random.randint(0, 59))
+    sixty = str(random.randint(1, 59))
     if len(sixty) == 1:
         sixty = "0" + sixty
+    sixty2 = str(random.randint(1, 59))
+    if len(sixty2) == 1:
+        sixty2 = "0" + sixty2
     new_date = str(random.randint(2025, 2025)) + '-' + twelve + '-' + thirty
-    new_time = twelve + ':' + sixty + ':' + sixty
+    new_time = twelve + ':' + sixty + ':' + sixty2
     rand_data.append([new_date, new_time, str(random.randint(1, 20)) + '.' + str(random.randint(0, 9)) + str(random.randint(0, 9))])
 
 rand_data.sort()
